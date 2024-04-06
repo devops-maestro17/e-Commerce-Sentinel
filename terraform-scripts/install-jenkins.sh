bae11ac89ffd59f6b512378c468e7537dec1dabe
Rajdeep Deogharia <148553140+devops-maestro17@users.noreply.github.com>
2024-04-06 13:19:35 +0000

    adding files

--- terraform-scripts/install-jenkins.sh
+++ terraform-scripts/install-jenkins.sh
@@ -28,3 +28,10 @@ sudo apt-get update
 sudo apt-get install -y jenkins
 sudo systemctl start jenkins
 sudo systemctl enable jenkins
+
+# Install Trivy
+sudo apt-get install wget apt-transport-https gnupg lsb-release
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
+echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get update
+sudo apt-get install -y trivy
